c7db1f5394991e29a83298ad7dae404d93a32264
inaki <iiaranzadi@gmail.com>
2016-02-09 12:46:49 -0800

    added index and app js code

---
+++ app.js
@@ -0,0 +1,10 @@
+angular.module('gistEditor', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider){
+
+
+})
+.controller('MainCtrl', function($scope, $http, $state, $stateParams){
+
+
+
+});
