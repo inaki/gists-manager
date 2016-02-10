fdf79fddb14cef1f79ee86db7066dc4ffe9bca24
inaki <iiaranzadi@gmail.com>
2016-02-09 20:18:05 -0800

    added some css transitions with ngAnimate and some more style

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module('gistEditor', ['ui.router','ui.ace'])
+angular.module('gistEditor', ['ui.router','ui.ace', 'ngAnimate'])
 .config(function($stateProvider, $urlRouterProvider){
 
       $urlRouterProvider.otherwise("/gist");
@@ -23,6 +23,8 @@ angular.module('gistEditor', ['ui.router','ui.ace'])
   // Simple GET request example:
   $http({
     method: 'GET',
+    url: 'https://api.github.com/users/inaki/gists?access_token=7344b082187ff090a6abbed986d4b28a3e18e9e6'
+    // url: 'https://api.github.com/users/clariture-test/gists?access_token=83c36d09aa7d3f91c867d12b30a6340af21e5397'
     // url: 'https://api.github.com/users/{user}/gists?access_token= '+ token +'';
   }).then(function successCallback(response) {
     $scope.gists = response.data;
@@ -58,6 +60,13 @@ angular.module('gistEditor', ['ui.router','ui.ace'])
         }
      };
 
+    // mark the active gist
+    $scope.selected = 0;
+
+      $scope.select= function(index) {
+         $scope.selected = index;
+         console.log(index);
+      };
 
 
 });
